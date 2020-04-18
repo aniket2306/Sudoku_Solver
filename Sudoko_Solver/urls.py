c1057c1a89ea6f6f75e3df2ba6a9459f5ea5e052
aniket2306 <39301505+aniket2306@users.noreply.github.com>
2020-04-18 13:34:29 +0530

    Update urls.py

--- Sudoko_Solver/urls.py
+++ Sudoko_Solver/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.conf.urls import url, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'sudoku'
 urlpatterns = [
